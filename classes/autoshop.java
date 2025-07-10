3b9c11fe07fe104758ef1ad0be132c7d690bed2f
zeekdocodes <clydeeeee19@gmail.com>
2025-07-10 13:45:37 +0800

    trying classes

---
+++ classes/autoshop.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class autoshop {
+    String brand;
+    int mile;
+
+    autoshop(){
+        mile = 0;
+
+    }
+
+    void det() {
+        System.err.println("The brand of the is " + brand + " and the milege of the given car is " + mile+" miles");
+
+    }
+}
